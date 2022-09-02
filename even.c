a4377f294b235a66a6c09a5cc149e9df94a2e31e
Anshu <varshneya521@gmail.com>
2022-09-02 23:38:18 +0530

    this is cloops program

---
+++ even.c
@@ -0,0 +1,12 @@
+/* print 1 to 100 */
+
+#include<stdio.h>
+int main()
+{
+	int i;
+	printf("even no from 1 to 100 are\n");
+	for (i=1;i<=50;i++){
+		printf("%d\n",2*i);
+	}
+	return 0;
+}
---
+++ numbers.c
@@ -0,0 +1,13 @@
+/* print number from 1 to 100 */
+
+#include<stdio.h>
+int main()
+{
+	int num,i;
+	printf("enter a value of num\n");
+	scanf("%d",&num);
+	for (i=1;i<101;i++){
+		printf("%d\n",i);
+	}
+	return 0;
+}
